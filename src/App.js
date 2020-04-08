7f17e62cc5fd2aedc5642ce9e4429ed8053b138c
Peter Gorgone <peter@peltronic.com>
2020-04-07 18:54:26 -0700

    [Section/#7-integrate-firebase-including-user-authorization] -- save auth user to Firebase DB

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import AuthPage from './pages/auth/auth.component';
 import Header from './components/header/header.component'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends Component {
 
@@ -20,8 +20,10 @@ class App extends Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(userAuth => {
+      //this.setState({ currentUser: user })
+      createUserProfileDocument(user)
+
     })
   }
 
