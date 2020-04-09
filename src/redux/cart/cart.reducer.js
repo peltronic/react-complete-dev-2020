09bfad4830334725bc675e9fe9658480d710ba62
Peter Gorgone <peter@peltronic.com>
2020-04-08 18:40:53 -0700

    [Section/#8-redux-integration-part-1] -- add items array to store items added to cart (redux)

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -2,11 +2,17 @@
 import { CartActionTypes } from './cart.types'
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: [...state.cartItems, action.payload]
+            }
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
