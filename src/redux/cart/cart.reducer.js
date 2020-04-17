dd8dd39956c1a559a98e66b8d05e4f49bcab8179
Peter Gorgone <peter@peltronic.com>
2020-04-16 18:43:55 -0700

    [Section/#8-redux-integration-part-1] -- added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import { CartActionTypes } from './cart.types'
-import { addItemToCart } from './cart.utils'
+import { addItemToCart, removeItemFromCart } from './cart.utils'
 
 const INITIAL_STATE = {
     hidden: true,
@@ -17,10 +17,21 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             //     ...state,
             //     cartItems: [...state.cartItems, action.payload]
             // }
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
                 hidden: !state.hidden
+            }
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                //cartItems: removeItemFromCart(state.cartItems, action.payload)
+                cartItems: state.cartItems.filter( iter => action.payload.id !== iter.id )
             }
         default:
             return state
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,12 +1,24 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existing = cartItems.find( iter => ( iter.id===cartItemToAdd.id ))
-    if (existing) {
-       return cartItems.map( (iter) => {
-           return ( iter.id===cartItemToAdd.id ) 
-            ? { ...iter, qty: iter.qty+1 } 
+   const existing = cartItems.find(iter => (iter.id === cartItemToAdd.id))
+   if (existing) {
+      return cartItems.map((iter) => {
+         return (iter.id === cartItemToAdd.id)
+            ? { ...iter, qty: iter.qty + 1 }
             : iter
-       }) 
-    } else {
-       return [ ...cartItems, { ...cartItemToAdd, qty: 1 } ]
-    }
+      })
+   } else {
+      return [...cartItems, { ...cartItemToAdd, qty: 1 }]
+   }
+}
+
+export const removeItemFromCart = (cartItems, itemToRemove) => {
+   const existing = cartItems.find( iter => iter.id===itemToRemove.id )
+
+   if ( existing.qty === 1 ) {
+      return cartItems.filter( iter => iter.id !== itemToRemove.id )
+   }
+
+   return cartItems.map( iter => {
+     return iter.id===itemToRemove.id ? {...iter, qty: iter.qty-1}  : iter
+   })
 }
